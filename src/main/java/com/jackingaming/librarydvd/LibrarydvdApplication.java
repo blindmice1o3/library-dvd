4f00353e054d5da3be3c91e6c4a7c58138a42f35
jamesgov <blindmice1o3@gmail.com>
2020-06-04 20:46:35 -0700

    first commit
    Ran my first Spring application.

---
+++ src/main/java/com/jackingaming/librarydvd/LibrarydvdApplication.java
@@ -0,0 +1,13 @@
+package com.jackingaming.librarydvd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarydvdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarydvdApplication.class, args);
+	}
+
+}
